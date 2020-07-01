b6224a1e17dc91b4e24ed328e1bc3d74e273b224
AladdinOmar61 <aladdin.omar@twgtech.com>
2020-07-01 15:14:08 -0400

    created post layout for frontend home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="App">
+      <div className="boxx-array">
+      <div className="boxx-container">
+        <div className="boxx"></div>
+        <div className="post-info">
+        <div className="profile-comment"></div>
+        <input className="boxx-comment" placeholder="caption"></input>
+        </div>
+      </div>
+      <div className="boxx-container">
+        <div className="boxx"></div>
+        <div className="post-info">
+        <div className="profile-comment"></div>
+        <input className="boxx-comment" placeholder="caption"></input>
+        </div>
+      </div>
+      <div className="boxx-container">
+        <div className="boxx"></div>
+        <div className="post-info">
+        <div className="profile-comment"></div>
+        <input className="boxx-comment" placeholder="caption"></input>
+        </div>
+      </div>
+      <div className="boxx-container">
+        <div className="boxx"></div>
+        <div className="post-info">
+        <div className="profile-comment"></div>
+        <input className="boxx-comment" placeholder="caption"></input>
+        </div>
+      </div>
+      </div>
+    </div>;
+  }
 }
 
 export default App;
