a53b413ab038c310a6acea67efbadfcdc4ecc14e
AladdinOmar61 <aladdin.omar@twgtech.com>
2020-07-02 16:38:44 -0400

    added all components of home page into branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -8,46 +8,57 @@ class App extends Component {
     this.state = {};
   }
   render() {
-    return <div className="App">
-      {/* <div className="header-flex">
-      <img className="pickboxx-logo" src={"../Logo.jpeg"}/>
-      <Link>Home</Link>
-      <Link>Explore</Link>
-      <Link>Marketplace</Link>
-      <Link>Map</Link>
-      <Link>Leaderboards</Link>
-      </div> */}
-      <div className="boxx-array">
-      <div className="boxx-container">
-        <div className="boxx"></div>
-        <div className="post-info">
-        <div className="profile-comment"></div>
-        <input className="boxx-comment" placeholder="caption"></input>
+    return (
+      <div className="App">
+        <div className="header-flex">
+          <img className="pickboxx-logo" src={"../Logo.jpeg"} />
+          <Link>Home</Link>
+          <Link>Explore</Link>
+          <Link>Marketplace</Link>
+          <Link>Map</Link>
+          <Link>Leaderboards</Link>
+        </div>
+        <div className="boxx-search-div">
+          <input className="boxx-search" type="text"></input>
+          <div className="boxx-nav">
+            <Link>#somethingPopular</Link>
+            <Link>Top Boxxes</Link>
+            <Link>Duels</Link>
+            <Link>Top Pickers</Link>
+          </div>
+        </div>
+        <div className="boxx-array">
+          <div className="boxx-container">
+            <div className="boxx"></div>
+            <div className="post-info">
+              <div className="profile-comment"></div>
+              <input className="boxx-comment" placeholder="caption"></input>
+            </div>
+          </div>
+          <div className="boxx-container">
+            <div className="boxx"></div>
+            <div className="post-info">
+              <div className="profile-comment"></div>
+              <input className="boxx-comment" placeholder="caption"></input>
+            </div>
+          </div>
+          <div className="boxx-container">
+            <div className="boxx"></div>
+            <div className="post-info">
+              <div className="profile-comment"></div>
+              <input className="boxx-comment" placeholder="caption"></input>
+            </div>
+          </div>
+          <div className="boxx-container">
+            <div className="boxx"></div>
+            <div className="post-info">
+              <div className="profile-comment"></div>
+              <input className="boxx-comment" placeholder="caption"></input>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="boxx-container">
-        <div className="boxx"></div>
-        <div className="post-info">
-        <div className="profile-comment"></div>
-        <input className="boxx-comment" placeholder="caption"></input>
-        </div>
-      </div>
-      <div className="boxx-container">
-        <div className="boxx"></div>
-        <div className="post-info">
-        <div className="profile-comment"></div>
-        <input className="boxx-comment" placeholder="caption"></input>
-        </div>
-      </div>
-      <div className="boxx-container">
-        <div className="boxx"></div>
-        <div className="post-info">
-        <div className="profile-comment"></div>
-        <input className="boxx-comment" placeholder="caption"></input>
-        </div>
-      </div>
-      </div>
-    </div>;
+    );
   }
 }
 
