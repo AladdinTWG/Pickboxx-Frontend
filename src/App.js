a476c0d668610ce4b5ea8d004c0cfdee5a9dbe78
Bo Yuan <bo.yuan@twgtech.com>
2020-07-13 18:41:20 -0700

    added_top_picker_page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TopBoxxes from "./components/TopBoxxes.js";
 import BoxxDetails from "./components/BoxxDetails.js";
 import Explore from "./components/Explore.js";
 import Leaderboards from "./components/Leaderboards.js"
-
+import TopPickerPage from "../../pickboxx-frontend/src/components/TopPickerPage";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,11 +23,14 @@ class App extends Component {
           <Link>Marketplace</Link>
           <Link>Map</Link>
           <Link to="/leaderboards">Leaderboards</Link>
-          <img
-            className="profile-icon"
-            src="https://www.greenecountyfoundation.org/wp-content/uploads/2019/09/Profile-Icon.png"
-            alt=""
-          />
+          <button className = "nav_user_btn">
+            <img
+              className="profile-icon"
+              src="https://www.greenecountyfoundation.org/wp-content/uploads/2019/09/Profile-Icon.png"
+              alt=""
+            />
+          </button>
+          
         </div>
         <div className="boxx-search-div">
           <input className="boxx-search" type="text"></input>
@@ -35,7 +38,7 @@ class App extends Component {
             <Link>#somethingPopular</Link>
             <Link to="TopBoxxes">Top Boxxes</Link>
             <Link>Duels</Link>
-            <Link>Top Pickers</Link>
+            <Link to="/TopPickers">Top Pickers</Link>
           </div>
         </div>
         <Route exact path="/">
@@ -53,9 +56,13 @@ class App extends Component {
         <Route exact path="/BoxxDetails">
           <BoxxDetails />
         </Route>
+        <Route exact path="/TopPickers">
+          <TopPickerPage />
+        </Route>
       </div>
     );
   }
 }
 
 export default App;
+
